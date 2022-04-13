0e0aa9beb54f850ab988bdf11460bb760ea2d781
MDakshithagithub <94457414+MDakshithagithub@users.noreply.github.com>
2022-04-13 10:41:15 +0530

    Update movie_sqlite.py

--- movie_sqlite.py
+++ movie_sqlite.py
@@ -9,21 +9,22 @@ c.execute("""CREATE TABLE movies (
 	actress text, 
     director text, 
     year integer
-	)""")
+	)""")#creating a table
 
 mov_1 = Movie('Radhe Shyam','Prabhas','Pooja Hegde','Radha Krishna Kumar',2022)
 mov_2 = Movie('Baahubali: The Beginning','Prabhas','Tamanna','S. S. Rajamouli',2015)
-mov_3= Movie('Vaikuntapuram','Allu Arjun','Pooja Hegde','Trivikram Srinivas',2020)
+mov_3= Movie('Vaikuntapuram','Allu Arjun','Pooja Hegde','Trivikram Srinivas',2020)#inserting 3 values
 def insert_mov(mov):
     with conn:
         c.execute("INSERT INTO movies VALUES (:name, :actor, :actress,:director,:year)", {'name': mov.name, 'actor': mov.actor, 'actress': mov.actress, 'director': mov.director, 'year': mov.year})
+
 insert_mov(mov_1)
 insert_mov(mov_2)
-insert_mov(mov_3)
+insert_mov(mov_3)#inserting into table
        
 c.execute("SELECT * FROM movies")
-print(c.fetchall())
+print(c.fetchall())# fetching all the rows from the table
 c.execute("SELECT name FROM movies WHERE actor='Prabhas'")
-print(c.fetchall())
+print(c.fetchall())# fetching movie name based on actor name
 conn.commit()
 conn.close()
